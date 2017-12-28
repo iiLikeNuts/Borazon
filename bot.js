6f77b7892762180d4149a5ef477b1f2a0b71b26f
iiLikeNuts <31368336+iiLikeNuts@users.noreply.github.com>
2017-12-28 15:33:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,42 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+ar Discord = require('discord.js');
+var rblx = require('roblox.js');
 
-client.on('ready', () => {
-    console.log('I am ready!');
+
+const client = new Discord.Client;
+client.on('ready',()>= {
+  console.log('i am ready');
 });
+rbx.login("Borazon_Bot","botnamednut")
+client.on('message',message => {
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+if(message.content === ":promote")
+let mod = message.guild.roles.find('name',"Trainer")
+if(message.member.roles.has(mod.id)) {
+  let user = args.slice(1).join(' ');
+const m = await message.channel.send('Promoting..');
+const id = rblx.getIdFromUsername(user);
+rblx.promote(2968801,id);
+m.edit('User should be ranked, if not, please DM <@329306122731257857>');
+} else {
+  return message.reply("You do not have permission to use that command, role required: Trainer");
+}
+};
+
 });
+client.on('message',message => {
 
-// THIS  MUST  BE  THIS  WAY
+if(message.content === ":demote")
+let mod = message.guild.roles.find('name',"Trainer")
+if(message.member.roles.has(mod.id)) {
+  let user = args.slice(1).join(' ');
+const m = await message.channel.send('Demoting..');
+const id = rblx.getIdFromUsername(user);
+rblx.demote(2968801,id);
+m.edit('User should be ranked, if not, please DM <@329306122731257857>');
+} else {
+  return message.reply("You do not have permission to use that command, role required: Trainer");
+}
+};
+
+});
 client.login(process.env.bottoken);
